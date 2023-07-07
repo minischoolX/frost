46da64bff00dd9ec55fb3652dcc7b6bffba4f8be
minischoolX <104305605+minischoolX@users.noreply.github.com>
2023-07-07 19:44:46 +0530

    Update AddOnsFragment.java

--- app/src/main/java/com/leotenebris/frostweb/AddOns/AddOnsFragment.java
+++ app/src/main/java/com/leotenebris/frostweb/AddOns/AddOnsFragment.java
@@ -1,5 +1,5 @@
 package com.leotenebris.frostweb.AddOns;
-
+/**
 import android.app.ActivityOptions;
 import android.content.Context;
 import android.content.Intent;
@@ -29,10 +29,120 @@ import java.util.Locale;
 
 import static com.leotenebris.frostweb.BrowserActivity.sharedPreferences;
 import static com.leotenebris.frostweb.BrowserActivity.tabFragments;
+*/
 
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+
+import com.leotenebris.frostweb.R;
+
+public class AddOnsFragment extends Fragment {
+    private SharedPreferences sharedPreferences;
+
+    public AddOnsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_add_ons, container, false);
+
+        // Initialize views
+        ImageView adBlockerImg = rootView.findViewById(R.id.adBlockerImg);
+        TextView adBlockerTitle = rootView.findViewById(R.id.adBlockerTitle);
+        TextView adBlockerDescription = rootView.findViewById(R.id.adBlockerDescription);
+        Switch adBlockerEnabled = rootView.findViewById(R.id.adBlockerEnabled);
+
+        ImageView videoEnhancerImg = rootView.findViewById(R.id.videoEnhancerImg);
+        TextView videoEnhancerTitle = rootView.findViewById(R.id.videoEnhancerTitle);
+        TextView videoEnhancerDescription = rootView.findViewById(R.id.videoEnhancerdescription);
+        Switch videoEnhancerEnabled = rootView.findViewById(R.id.videoEnhancerEnabled);
+        TextView videoEnhancerInfo = rootView.findViewById(R.id.videoEnhancerInfo);
+        ImageView videoEnhancerReloadImg = rootView.findViewById(R.id.videoEnhancerReloadImg);
+
+        ImageView readAloudImg = rootView.findViewById(R.id.readAloudImg);
+        TextView readAloudTitle = rootView.findViewById(R.id.readAloudTitle);
+        TextView readAloudDescription = rootView.findViewById(R.id.readAloudDescription);
+        Switch readAloudEnabled = rootView.findViewById(R.id.readAloudEnabled);
+
+        // Initialize SharedPreferences
+        sharedPreferences = getActivity().getSharedPreferences("AddOnsPrefs", getActivity().MODE_PRIVATE);
+
+        // Set initial switch state from SharedPreferences
+        adBlockerEnabled.setChecked(sharedPreferences.getBoolean("adBlocker", true));
+        videoEnhancerEnabled.setChecked(sharedPreferences.getBoolean("videoEnhancer", true));
+        readAloudEnabled.setChecked(sharedPreferences.getBoolean("readAloud", true));
+
+        // Set switch change listeners
+        adBlockerEnabled.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // Store the new switch state in SharedPreferences
+                sharedPreferences.edit().putBoolean("adBlocker", isChecked).apply();
+            }
+        });
+
+        videoEnhancerEnabled.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // Store the new switch state in SharedPreferences
+                sharedPreferences.edit().putBoolean("videoEnhancer", isChecked).apply();
+
+                if (isChecked) {
+                    videoEnhancerInfo.setVisibility(View.VISIBLE);
+                    videoEnhancerInfo.setText("Video Enhancer is configured with default settings. You can click anywhere on this card to modify your preferences.");
+                    //videoEnhancerReloadImg.setVisibility(View.VISIBLE);
+                    //videoEnhancerReloadImg.setImageResource(R.drawable.video_enhancer_reload);
+                    videoEnhancerImg.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            // Perform the desired action when the Video Enhancer card is clicked
+                            Toast.makeText(getActivity(), "Video Enhancer card clicked!", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } else {
+                    videoEnhancerInfo.setVisibility(View.GONE);
+                    //videoEnhancerReloadImg.setVisibility(View.GONE);
+                    videoEnhancerImg.setOnClickListener(null);
+                }
+                
+            }
+        });
+
+        readAloudEnabled.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // Store the new switch state in SharedPreferences
+                sharedPreferences.edit().putBoolean("readAloud", isChecked).apply();
+            }
+        });
+
+        
+//        videoEnhancerInfo.setVisibility(View.GONE);
+//        videoEnhancerInfo.setVisibility(View.VISIBLE);
+//        videoEnhancerInfo.setText("Video Enhancer");
+//        videoEnhancerReloadImg.setVisibility(View.GONE);
+//        videoEnhancerReloadImg.setVisibility(View.VISIBLE);
+//        videoEnhancerReloadImg.setImageResource(R.drawable.video_enhancer_reload);
+        
+
+        return rootView;
+    }
+}
 /**
  * A simple {@link Fragment} subclass.
  */
+/**
 public class AddOnsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
     public static final String KEY_LANGUAGE = "language";
     public static final String KEY_THEME = "theme";
@@ -231,3 +341,4 @@ public class AddOnsFragment extends PreferenceFragmentCompat implements SharedPr
         getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
     }
 }
+*/
