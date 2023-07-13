1877e2cb3a588317d3b014ee0d6d478617f74796
minischoolX <104305605+minischoolX@users.noreply.github.com>
2023-07-13 13:36:48 +0530

    Update FrostWebApplication.java

--- app/src/main/java/com/leotenebris/frostweb/FrostWebApplication.java
+++ app/src/main/java/com/leotenebris/frostweb/FrostWebApplication.java
@@ -1,6 +1,7 @@
 package com.leotenebris.frostweb;
 
 import android.app.Application;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.util.Log;
@@ -15,7 +16,7 @@ import java.util.ArrayList;
 public class FrostWebApplication extends Application {
     private static FrostWebApplication instance;
 
-    private JsManager jsManager = new JsManager();
+    private JsManager jsManager = new JsManager(Context);
 
     public static final String KEY_TABS = "Tabs";
     public static final String KEY_HISTORY = "History";
